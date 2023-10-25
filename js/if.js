b1f24a9a19b73cfe0685babbd678f60da27ace8f
Robert Chamberlain <robert.chamberlain@cmeducations.se>
2023-10-25 16:04:17 +0200

    Initializing repo

---
+++ js/if.js
@@ -0,0 +1,15 @@
+// don't touch the window.addEventListener line, or the closing }); at the end of the file
+window.addEventListener('load', function () {  
+
+/*   
+Complete each exercise, test by running if.html, then comment out the code to stop it running again when you move on to subsequent exercises. 
+
+If you don't the prompts and alerts from previous exercises will run whenever you reload the page
+
+Use the exercise number as the variable for the prompt.
+
+Eg:
+let exercise1 = prompt("Enter a number");
+*/
+
+});
