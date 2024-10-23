8cab36cad077f7da6e601e49f56dd2044f78b58f
May Sunktong <pattaranan.sunktong@gmail.com>
2024-10-24 00:37:58 +0200

    question 1-9

--- js/if.js
+++ js/if.js
@@ -1,7 +1,6 @@
 // don't touch the window.addEventListener line, or the closing }); at the end of the file
-window.addEventListener('load', function () {  
-
-/*   
+window.addEventListener("load", function () {
+  /*   
 Complete each exercise, test by running if.html, then comment out the code to stop it running again when you move on to subsequent exercises. 
 
 If you don't the prompts and alerts from previous exercises will run whenever you reload the page
@@ -11,5 +10,74 @@ Use the exercise number as the variable for the prompt.
 Eg:
 let exercise1 = prompt("Enter a number");
 */
+  // exercise 1
+  // let exercise1 = prompt("Exercise 1: Enter a number");
+  // if (exercise1 > 5) {
+  //   alert("Your entered number is greater than 5.")
+  // } else {
+  //   this.alert("Your entered number is equal or less than 5.")
+  // }
+  // exercise 2
+  // let exercise2 = prompt("Exercise 2: Enter a number");
+  // if (exercise2 > 5){
+  //   alert("The number is greater than 5.");
+  // } else {
+  //   alert("The number is less than 5.");
+  // }
+  // exercise 3
+  // let exercise3 = prompt("Exercise 3: Enter a number");
+  // if (exercise3 > 5) {
+  //   alert("The number is greater than 5.");
+  // } else if (exercise3 < 5) {
+  //   alert("The number is less than 5.");
+  // } else {
+  //   alert("The number is equal to 5.");
+  // }
+  // exercise 4
+  // let exercise4 = prompt("Exercise 4: Enter a number"); // string
+  // let exercise4 = parseInt(prompt("Exercise 4: Enter a number")); // number
+  // let tenTimedNumber = exercise4 * 10;
+  // alert(
+  //   `Ten times of the number is ${tenTimedNumber}. Type of the number is ${typeof exercise4}`
+  // );
+  // exercise 5
+  // let exercise5 = prompt("Exercise 5: Enter a number");
+  // if (exercise5 >= 5 && exercise5 <= 10) {
+  //   alert("The number is between 5 and 10.");
+  // } else {
+  //   alert("The number is NOT between 5 and 10")
+  // }
+  // exercise 6
+  // let exercise6 = parseInt(prompt("Exercise 6: Enter a number"));
+  // let result = (exercise6 < 5 || exercise6 > 10) ? "YES" : "NO";
+  // alert(result)
+  // exercise 7
+  // let exercise7 = parseInt(prompt("Exercise 7: Enter a number"));
+  // if (exercise7 < 5) {
+  //   alert("The number is less than 5.");
+  // } else if (exercise7 > 10) {
+  //   alert("The number is greater than 10.");
+  // } else {
+  //   alert("The number is neither less than 5 nor greater than 10.");
+  // }
+  // exercise 8
+  // let exercise8_NumberOne = parseInt(
+  //   prompt("Exercise 8: Enter the first number")
+  // );
+  // let exercise8_NumberTwo = parseInt(
+  //   prompt("Exercise 8: Enter the second number")
+  // );
+  // if (exercise8_NumberOne > exercise8_NumberTwo) {
+  //   alert("The first number is greater than the second number.");
+  // } else if (exercise8_NumberOne < exercise8_NumberTwo) {
+  //   alert("The first number is less than the second number.");
+  // } else {
+  //   alert("The first number is equal to the second number.");
+  // }
+
+  // exercise 9
+  
+
+
 
 });
